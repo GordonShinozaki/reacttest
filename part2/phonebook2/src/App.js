a5ea4fff8ff5abc976c833a6d4883c39c685d3a5
GordonShinozaki <44336201+GordonShinozaki@users.noreply.github.com>
2021-08-10 22:09:24 +0900

    used Axios to allow updates to be saved to a backend server

--- part2/phonebook2/src/App.js
+++ part2/phonebook2/src/App.js
@@ -1,71 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Filter from './components/Filter'
-import NewForm from './components/NewForm'
-import Numbers from './components/Numbers'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import NewForm from "./components/NewForm";
+import Numbers from "./components/Numbers";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ newfilter, setNewFilter] = useState('')
-  const [ results, setResults] = useState([])
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newfilter, setNewFilter] = useState("");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-        setResults(response.data)
-      })
-  }, [])
-  console.log('render', persons, 'data')
-  
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setResults(response.data);
+    });
+  }, []);
+  console.log("render", persons, "data");
+
   const handleFilter = (event) => {
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
   const handleNamechange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
   const handleNumberchange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-  const addPerson = (event) => { //addname is called via an event 
-    event.preventDefault() //stops page from automatically submitting shit 
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+  const addPerson = (event) => {
+    //addname is called via an event
+    event.preventDefault(); //stops page from automatically submitting shit
     const nameObject = {
       name: newName,
-      number: newNumber
-    }
-    if (persons.map(person => person.name).includes(nameObject.name)){
-      window.alert(`${nameObject.name} is already added to phonebook`)
+      number: newNumber,
+    };
+    if (persons.map((person) => person.name).includes(nameObject.name)) {
+      window.alert(`${nameObject.name} is already added to phonebook`);
     } else {
-    setPersons(persons.concat(nameObject))
-    setResults(results.concat(nameObject))
-    setNewName('')
-    setNewNumber('') //reset button states
+      axios
+        .post("http://localhost:3001/persons", nameObject)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+          setResults(results.concat(response.data));
+          setNewName("");
+          setNewNumber(""); //reset button states
+        });
     }
-  }
+  };
 
-  const filterimp = (event) =>{
-    event.preventDefault()
-    const perstoshow = persons.filter(person => person.name.toLowerCase().includes(newfilter.toLowerCase()))
-    setResults(perstoshow)
-    setNewFilter('')
-  }
+  const filterimp = (event) => {
+    event.preventDefault();
+    const perstoshow = persons.filter((person) =>
+      person.name.toLowerCase().includes(newfilter.toLowerCase())
+    );
+    setResults(perstoshow);
+    setNewFilter("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter newfilter={newfilter} handleFilter = {handleFilter} filterimp = {filterimp} />
+      <Filter
+        newfilter={newfilter}
+        handleFilter={handleFilter}
+        filterimp={filterimp}
+      />
       <h2>Add a New</h2>
-      <NewForm name = {newName} namechange = {handleNamechange} number = {newNumber} numberchange = {handleNumberchange} clickhandle = {addPerson}/>
+      <NewForm
+        name={newName}
+        namechange={handleNamechange}
+        number={newNumber}
+        numberchange={handleNumberchange}
+        clickhandle={addPerson}
+      />
       <h2>Numbers</h2>
-      <Numbers listofthings = {results} />
+      <Numbers listofthings={results} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
